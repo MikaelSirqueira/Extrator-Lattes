9316ce6050e461c45c425c983965596d8b359d02
Mikael Sirqueira <silmike14@yahoo.com.br>
2024-08-20 19:55:32 -0300

    feat: extract menu

--- src/shared/components/FilterPanel/index.jsx
+++ src/shared/components/FilterPanel/index.jsx
@@ -1,52 +1,99 @@
-import React from 'react';
-import { Card, CardContent, TextField, InputAdornment, Button, Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardContent, TextField, InputAdornment, Button, Box, Backdrop, CircularProgress } from '@mui/material';
+import { DataAccordion } from '../DataAccordion';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import styles from './styles';
 
 export function FilterPanel() {
+  const [showData, setShowData] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleExtractClick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setShowData(true);
+    }, 2000); // Simula um tempo de carregamento
+  };
+
+  const handleBackClick = () => {
+    setShowData(false);
+  };
+
   return (
-    <Box sx={styles.container}>
-      <Card sx={styles.card}>   
-        <CardContent bgcolor='secondary.dark'>
-          <Box sx={styles.textFieldContainer}>
-            <Box sx={styles.textFieldWrapper}>
-              <TextField 
-                placeholder="Nome completo"
-                sx={styles.textField}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <PersonOutlineIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Typography sx={styles.textDown}>
-                Insira o nome completo do primeiro pesquisador
-              </Typography>
+    <>
+      <Box sx={styles.container}>
+        <Card sx={styles.card}>
+          <CardContent bgcolor='secondary.dark'>
+            <Box sx={styles.textFieldContainer}>
+              <Box sx={styles.textFieldWrapper}>
+                <TextField 
+                  placeholder="Nome completo"
+                  sx={{
+                    '& .MuiFormHelperText-root': { ml: '0', fontSize: 13 },
+                    '& .MuiInputBase-root': { backgroundColor: '#FFF' }
+                  }}
+                  helperText='Insira o nome completo do primeiro pesquisador'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <PersonOutlineIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
+
+              <Box sx={styles.textFieldWrapper}>
+                <TextField
+                  placeholder="Nome completo"
+                  sx={{
+                    '& .MuiFormHelperText-root': { ml: '0', fontSize: 13 },
+                    '& .MuiInputBase-root': { backgroundColor: '#FFF' },
+                    borderRadius: 2,
+                  }}                
+                  helperText='Insira o nome completo do segundo pesquisador'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <PersonOutlineIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>       
             </Box>
-            <Box sx={styles.textFieldWrapper}>
-              <TextField
-                placeholder="Nome completo"
-                sx={styles.textField}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <PersonOutlineIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Typography sx={styles.textDown}>
-                Insira o nome completo do segundo pesquisador
-              </Typography>
-            </Box>       
+          </CardContent>
+          <Box>
+            <Button 
+              variant='contained' 
+              color="primary" 
+              size="large"
+              sx={{ borderRadius: 24, textTransform: 'none', m: '16px 40px',
+                '& .Mui-disabled': { color: '#FFF' },
+              }}      
+              onClick={handleExtractClick}
+              disabled={showData || loading}
+            >
+              Extrair
+            </Button>
           </Box>
-        </CardContent>
-        <Box>
-          <Button size="large" sx={styles.buttonContainer}>Extrair</Button>
-        </Box>
-      </Card>
-    </Box>
+        </Card>
+      </Box>
+
+      {showData && 
+        <DataAccordion onBackClick={handleBackClick} />
+      }
+
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1,
+          display: 'flex', gap: 4
+         }}
+        open={loading}
+      >
+        Carregando
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
   );
 }
--- src/shared/pages/extract-data/index.jsx
+++ src/shared/pages/extract-data/index.jsx
@@ -1,82 +1,67 @@
 import { Box, Button, Divider, Link, Typography } from "@mui/material";
 import { BodyTitle } from "../../components/BodyTitle";
 import { useNavigate } from "react-router-dom";
-import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import { SavedSearchs } from "../../components/SavedSearchs";
+import { useState } from "react";
+import Researchers from "./researchers";
+import { FilterPanel } from "../../components/FilterPanel";
+import { SearchResults } from "../../components/SearchResults";
+import { DataAccordion } from "../../components/DataAccordion";
 
 export function ExtractData() {
   const navigate = useNavigate();
+  const [isSelectedToExtract, setIsSelectedToExtract] = useState(false);
 
-  const searchs = [
-    {
-      title: 'Emerson Paraiso e Jean Barddal - 03/06/2024',
-      link: 'https://www.google.com.br',
-    },
-    {
-      title: 'Emerson Paraiso e Jean Barddal - 03/06/2024',
-      link: 'https://www.google.com.br',
-    },
-    
-  ];
+  function showPostGraduateProgram() {
+    setIsSelectedToExtract(true);
+  } 
+
+  function showResearchers() {
+    setIsSelectedToExtract(true);
+  }
 
   return (
     <Box p={8}>
       <BodyTitle />
-    
-      <Box 
-        mb={4}
-        display={'flex'}
-        alignItems='center'
-        justifyContent='center'
-        sx={{ gap: 8 }}
-      >
-        <Button 
-          variant='contained' 
-          size="large" 
-          color="primary"
-          sx={{ textTransform: 'none', borderRadius: '24px' }}    
-          onClick={() => navigate('/about')}       
-        >
-          Extrair dados de um pesquisador
-        </Button>
-        <Button 
-          variant='contained' 
-          size="large" 
-          color="primary" 
-          sx={{ textTransform: 'none', borderRadius: '24px' }}
-          onClick={() => navigate('/extract')}          
-        >
-          Extrair dados de um programa
-        </Button>
-      </Box>
 
-      <Divider aria-hidden="true" />
-
-      <Box component={'section'} mt={4}>
-        <Typography variant="subtitle1" color='secondary.dark' mb={3}>
-          Pesquisas salvas  
-        </Typography>
-        {searchs.length > 0 ? (
-          <Box component={'div'}>
-            {searchs.map((search, index) => (
-              <Link 
-                key={index}
-                color='customComponents.dark' 
-                href={search.link} 
-                variant="overline"
-                display={'flex'}
-                alignItems={'center'}
-                textTransform={'none'}
-                gap={1}
-                mb={2}
-              >
-                {search.title} <ArrowOutwardIcon sx={{fontSize: 16}} />
-              </Link>
-            ))}
+      {!isSelectedToExtract ? (
+        <>
+          <Box 
+            mb={4}
+            display={'flex'}
+            alignItems='center'
+            justifyContent='center'
+            sx={{ gap: 8 }}
+          >
+            <Button 
+              variant='contained' 
+              size="large" 
+              color="primary"
+              sx={{ textTransform: 'none', borderRadius: '24px' }}    
+              onClick={showResearchers}       
+            >
+              Extrair dados de um pesquisador
+            </Button>
+            <Button 
+              variant='contained' 
+              size="large" 
+              color="primary" 
+              sx={{ textTransform: 'none', borderRadius: '24px' }}
+              onClick={showPostGraduateProgram}          
+            >
+              Extrair dados de um programa
+            </Button>
           </Box>
-        ) : (
-          <Typography color='secondary.light'> Não há pesquisas salvas no momento </Typography>
-        )}
-      </Box>
+
+          <Divider aria-hidden="true" />
+
+          <SavedSearchs />
+        </>
+      ): (
+        <>
+          <FilterPanel />          
+        </>
+      )}
     </Box>
   );
 }
