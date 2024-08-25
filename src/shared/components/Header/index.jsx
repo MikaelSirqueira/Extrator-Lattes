ae85071e80f98e6f7aa0b9edc1a7493fa8b241eb
Mikael Sirqueira <silmike14@yahoo.com.br>
2024-08-24 23:37:30 -0300

    fix: adjust theme after reload

--- src/shared/components/Header/index.jsx
+++ src/shared/components/Header/index.jsx
@@ -6,53 +6,52 @@ import styles from './styles';
 import { useAppThemeContext } from '../../contexts';
 import { useNavigate } from 'react-router-dom';
 
-
 export function Header({isLoggedIn}) {
-
   const { themeName, toggleTheme } = useAppThemeContext();
   const navigate = useNavigate();
+
+  const logoPucpr = themeName === 'light'  ? LogoLight : LogoDark;
 
   const handleNavigation = (hash) => {
     navigate(`/about${hash}`, { replace: true });
   };
-
-  const logoPucpr = themeName === 'light' ? LogoLight : LogoDark;
   
   return (
-  <>
-    { isLoggedIn ? (
-      
-      <Box sx={styles.headerContainer}>
-        <Box sx={styles.imageContainer} onClick={() => navigate('/home')}>
-          <img src={logoPucpr} alt="Logo da PUCPR" style={styles.logo} />
-        </Box>
-        <Box sx={styles.menuContainer}>
-          <Button color="secondary" onClick={() => handleNavigation('#about')} sx={styles.button}>Sobre</Button>
-          <Button color="secondary" onClick={() => handleNavigation('#guide')} sx={styles.button}>Manual</Button>
-          <Button color="secondary" sx={styles.button} onClick={toggleTheme}>Alto Contraste</Button>
-          <Button color="primary" variant='contained' onClick={() => navigate('/extract')} sx={styles.button}>Extrair</Button>
-        </Box>
-      </Box>
-    ) : (
-      <Box sx={styles.headerContainer}>
-      <Box sx={{
-          flexGrow: 1,
-          display: 'flex', 
-          alignItems: 'center'
-        }}>
-          <img src={logoPucpr} alt="Logo da PUCPR" style={styles.logo} />
-      </Box>
-      <Box sx={styles.menuContainer}>
-       <Button color="secondary" sx={{
-          borderRadius: '24px',
-          textTransform: 'none',
-          alignItems: 'right'
-        }} onClick={toggleTheme}>Alto Contraste</Button>
-      </Box>
+  <>      
+    <Box sx={styles.headerContainer}>
+
+      { isLoggedIn ? (
+        <>
+          <Box sx={styles.imageContainer} onClick={() => navigate('/home')}>
+            <img src={logoPucpr} alt="Logo da PUCPR" style={styles.logo} />
+          </Box>
+          <Box sx={styles.menuContainer}>
+            <Button color="secondary" onClick={() => handleNavigation('#about')} sx={styles.button}>Sobre</Button>
+            <Button color="secondary" onClick={() => handleNavigation('#guide')} sx={styles.button}>Manual</Button>
+            <Button color="secondary" sx={styles.button} onClick={toggleTheme}>Alto Contraste</Button>
+            <Button color="primary" variant='contained' onClick={() => navigate('/extract')} sx={styles.button}>Extrair</Button>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box sx={{
+            flexGrow: 1,
+            display: 'flex', 
+            alignItems: 'center'
+          }}>
+            <img src={logoPucpr} alt="Logo da PUCPR" style={styles.logo} />
+          </Box>
+          <Box sx={styles.menuContainer}>
+            <Button color="secondary" sx={{
+              borderRadius: '24px',
+              textTransform: 'none',
+              alignItems: 'right'
+            }} onClick={toggleTheme}>Alto Contraste</Button>
+          </Box>
+        </>
+      )}
+
     </Box>
-    )
-  }
   </>
   );
 }
-  
